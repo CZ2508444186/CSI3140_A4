741ea3026cd9b5f91b0aaa8ae4a03960603884d7
Wenbo <164053327+CZ2508444186@users.noreply.github.com>
2024-08-01 07:38:27 +0800

    Add files via upload

---
+++ Assignment_4/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost'; // or your host
+$dbname = 'triage_db';
+$username = 'root'; // or your db username
+$password = 'Y17386009957'; // or your db password
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+?>
